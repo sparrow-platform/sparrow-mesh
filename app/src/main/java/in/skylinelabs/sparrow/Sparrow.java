33e44bf23a3676b9b221da631ff4ddc3d7155e32
Jay Lohokare <jaylohokare@gmail.com>
2019-05-08 21:24:17 -0400

    Added code to refresh WifiDirect connections

--- app/src/main/java/in/skylinelabs/sparrow/Sparrow.java
+++ app/src/main/java/in/skylinelabs/sparrow/Sparrow.java
@@ -16,6 +16,7 @@ import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
@@ -48,20 +49,22 @@ public class Sparrow extends Service {
 
     WifiP2pManager manager;
     WifiP2pManager.Channel channel;
-
     WifiManager mWifiManager;
+
+    int SERVICE_BROADCASTING_INTERVAL = 5000;
+    int SERVICE_DISCOVERING_INTERVAL = 5000;
+    WifiP2pDnsSdServiceInfo serviceInfo;
+    WifiP2pDnsSdServiceRequest serviceRequest;
+    final HashMap<String, String> wifiNeighbours = new HashMap<String, String>();
+
+    final Handler broadcastHandler = new Handler();
+    final Handler mServiceDiscoveringHandler = new Handler();
 
     WifiManager.WifiLock mWifiLock = null;
 
 
-    WifiP2pManager.ActionListener broadcastACtionListner;
-    WifiP2pManager.ActionListener discoveryActionLisner;
-    WifiP2pManager.ActionListener serviceRequestActionListner;
-
-    WifiP2pDnsSdServiceRequest serviceRequest;
-
-    final HashMap<String, String> wifiNeighbours = new HashMap<String, String>();
-
+    WifiP2pManager.ActionListener broadcastPeerDiscoveryActionListner;
+    WifiP2pManager.ActionListener broadcastActionListner;
 
     @Override
     public void onCreate() {
@@ -80,18 +83,18 @@ public class Sparrow extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
-        mWifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
         startSparrowWifiService();
+
         startTimer();
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
-
+        stopSparrowWifiService();
         stoptimertask();
+        super.onDestroy();
     }
 
     private void sendMessegeToActivity(String message) throws IOException {
@@ -104,118 +107,183 @@ public class Sparrow extends Service {
 
 
     /**************************************WIFI********************************************/
-    private void startSparrowWifiService() {
 
-        /*******************BROADCAST****************/
+    private void stopSparrowWifiService(){
+        if( mWifiLock == null )
+            Log.w(TAG_SPARROW_WIFI_SERVICE, "WifiLock was not created previously");
+
+        if( mWifiLock != null && mWifiLock.isHeld() ){
+            mWifiLock.release();
+        }
+
+        stopBroadcastingService();
+    }
+
+    public void stopBroadcastingService(){
+        mWifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+
+        manager.clearLocalServices(channel, broadcastPeerDiscoveryActionListner);
+        manager.stopPeerDiscovery(channel, broadcastPeerDiscoveryActionListner);
+        manager.clearServiceRequests(channel, broadcastPeerDiscoveryActionListner);
+
+        manager.clearLocalServices(channel, broadcastActionListner);
+        manager.stopPeerDiscovery(channel, broadcastActionListner);
+        manager.clearServiceRequests(channel, broadcastActionListner);
+    }
+
+
+    private void startSparrowWifiService() {
+        mWifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if( mWifiLock == null )
+            mWifiLock = mWifiManager.createWifiLock(WifiManager.WIFI_MODE_FULL, TAG_SPARROW_WIFI_SERVICE);
+
+        mWifiLock.setReferenceCounted(false);
+
+        if( !mWifiLock.isHeld() )
+            mWifiLock.acquire();
+
+
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+
         Map record = new HashMap();
         record.put("name", "Sparrow");
         record.put("data", "PUT DATA HERE");
 
         String name = "SPARROW" + getRandomString(10);
 
-        WifiP2pDnsSdServiceInfo serviceInfo =
-                WifiP2pDnsSdServiceInfo.newInstance(name, "Communication", record);
+        serviceInfo = WifiP2pDnsSdServiceInfo.newInstance(name, "Communication", record);
 
         try {
             sendMessegeToActivity("My name is " + name);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        channel = manager.initialize(this, getMainLooper(), null);
 
-        broadcastACtionListner = new WifiP2pManager.ActionListener() {
+        startBroadcastingService();
+        prepareServiceDiscovery();
+        startServiceDiscovery();
+
+
+    }
+
+    public void startBroadcastingService(){
+
+        stopBroadcastingService();
+
+        broadcastActionListner = new WifiP2pManager.ActionListener() {
+
             @Override
-            public void onSuccess() {Log.d(TAG_SPARROW_WIFI_SERVICE, "Added P2P service");}
+            public void onSuccess() {
+                // service broadcasting started
 
-            @Override
-            public void onFailure(int arg0) {Log.d(TAG_SPARROW_WIFI_SERVICE, "Failed to add P2P service");}
-        };
-        manager.addLocalService(channel, serviceInfo, broadcastACtionListner);
-
-
-        /*******************BROADCAST****************/
-
-
-        /*******************DISCOVERY****************/
-        WifiP2pManager.DnsSdTxtRecordListener txtListener = (fullDomain, record1, device) -> {
-            Log.d(TAG_SPARROW_WIFI_SERVICE, "Received data from Sparrow net" + record1.toString());
-            wifiNeighbours.put(device.deviceAddress, record1.get("name").toString());
-        };
-
-        WifiP2pManager.DnsSdServiceResponseListener servListener = (instanceName, registrationType, resourceType) -> {
-            resourceType.deviceName = wifiNeighbours
-                    .containsKey(resourceType.deviceAddress) ? wifiNeighbours
-                    .get(resourceType.deviceAddress) : resourceType.deviceName;
-            Log.d(TAG_SPARROW_WIFI_SERVICE, "Device name  " + instanceName);
-            try {
-                sendMessegeToActivity(instanceName);
-            } catch (IOException e) {
-                e.printStackTrace();
+                broadcastHandler
+                        .postDelayed(mServiceBroadcastingRunnable,
+                                SERVICE_BROADCASTING_INTERVAL);
             }
 
+            @Override
+            public void onFailure(int error) {
+                // react to failure of adding the local service
+                Log.d(TAG_SPARROW_WIFI_SERVICE, "Failed to clear old WifiP2p");
+            }
         };
 
-        manager.setDnsSdResponseListeners(channel, servListener, txtListener);
+        manager.addLocalService(channel, serviceInfo,broadcastActionListner);
+    }
+
+    private Runnable mServiceBroadcastingRunnable = new Runnable() {
+        @Override
+        public void run() {
+            broadcastPeerDiscoveryActionListner = new WifiP2pManager.ActionListener() {
+                @Override
+                public void onSuccess() {
+                }
+
+                @Override
+                public void onFailure(int error) {
+                }
+            };
+
+            manager.discoverPeers(channel, broadcastPeerDiscoveryActionListner);
+            broadcastHandler
+                    .postDelayed(mServiceBroadcastingRunnable, SERVICE_BROADCASTING_INTERVAL);
+        }
+    };
+
+    private Runnable mServiceDiscoveringRunnable = new Runnable() {
+        @Override
+        public void run() {
+            startServiceDiscovery();
+        }
+    };
+
+    public void prepareServiceDiscovery() {
+        manager.setDnsSdResponseListeners(channel,
+                (instanceName, registrationType, srcDevice) -> {
+                    // do all the things you need to do with detected service
+                    srcDevice.deviceName = wifiNeighbours
+                            .containsKey(srcDevice.deviceAddress) ? wifiNeighbours
+                            .get(srcDevice.deviceAddress) : srcDevice.deviceName;
+                    Log.d(TAG_SPARROW_WIFI_SERVICE, "Device name  " + instanceName);
+                    try {
+                        sendMessegeToActivity(instanceName);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }, (fullDomainName, record, device) -> {
+                            Log.d(TAG_SPARROW_WIFI_SERVICE, "Received data from Sparrow net" + record.toString());
+                            wifiNeighbours.put(device.deviceAddress, record.get("name").toString());
+                    // do all the things you need to do with detailed information about detected service
+                });
 
         serviceRequest = WifiP2pDnsSdServiceRequest.newInstance();
-
-        serviceRequestActionListner = new WifiP2pManager.ActionListener() {
-            @Override
-            public void onSuccess() {Log.d(TAG_SPARROW_WIFI_SERVICE, "Service request Success");}
-
-            @Override
-            public void onFailure(int code) {Log.d(TAG_SPARROW_WIFI_SERVICE, "Service request Failuer");}
-        };
-        manager.addServiceRequest(channel, serviceRequest,serviceRequestActionListner);
-
-
-        discoveryActionLisner = new WifiP2pManager.ActionListener() {
-            @Override
-            public void onSuccess() {Log.d(TAG_SPARROW_WIFI_SERVICE, "Discover services  Success");}
-
-            @Override
-            public void onFailure(int code) {
-                if (code == WifiP2pManager.P2P_UNSUPPORTED) {
-                    Log.d(TAG_SPARROW_WIFI_SERVICE, "P2P isn't supported on this device.");
-                }
-            }
-        };
-        manager.discoverServices(channel, discoveryActionLisner);
-        /*******************DISCOVERY****************/
     }
 
-    /***
-     * Calling this method will aquire the lock on wifi. This is avoid wifi
-     * from going to sleep as long as <code>releaseWifiLock</code> method is called.
-     **/
-    private void holdWifiLock() {
-        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+    private void startServiceDiscovery() {
+        manager.removeServiceRequest(channel, serviceRequest,
+                new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        manager.addServiceRequest(channel, serviceRequest,
+                                new WifiP2pManager.ActionListener() {
 
-        if( mWifiLock == null )
-            mWifiLock = wifiManager.createWifiLock(WifiManager.WIFI_MODE_FULL, TAG_SPARROW_WIFI_SERVICE);
+                                    @Override
+                                    public void onSuccess() {
+                                        manager.discoverServices(channel,
+                                                new WifiP2pManager.ActionListener() {
 
-        mWifiLock.setReferenceCounted(false);
+                                                    @Override
+                                                    public void onSuccess() {
+                                                        //service discovery started
 
-        if( !mWifiLock.isHeld() )
-            mWifiLock.acquire();
+                                                        mServiceDiscoveringHandler.postDelayed(
+                                                                mServiceDiscoveringRunnable,
+                                                                SERVICE_DISCOVERING_INTERVAL);
+                                                    }
+
+                                                    @Override
+                                                    public void onFailure(int error) {
+                                                        // react to failure of starting service discovery
+                                                    }
+                                                });
+                                    }
+
+                                    @Override
+                                    public void onFailure(int error) {
+                                        // react to failure of adding service request
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onFailure(int reason) {
+                        // react to failure of removing service request
+                    }
+                });
     }
-
-    /***
-     * Calling this method will release if the lock is already help. After this method is called,
-     * the Wifi on the device can goto sleep.
-     **/
-    private void releaseWifiLock() {
-
-        if( mWifiLock == null )
-
-            if( mWifiLock != null && mWifiLock.isHeld() ){
-                mWifiLock.release();
-                //mWifiLock = null;
-            }
-
-    }
-
 
     /**************************************WIFI********************************************/
 
@@ -239,25 +307,12 @@ public class Sparrow extends Service {
     public void initializeTimerTask() {
         timerTask = new TimerTask() {
             public void run() {
-                Log.i(TAG_SPARROW_WIFI_SERVICE, "in timer ++++  ");
+
                 if(mWifiManager.isWifiEnabled()==false)
                 {
                     mWifiManager.setWifiEnabled(true);
                 }
-                holdWifiLock();
-                manager.clearLocalServices(channel,broadcastACtionListner);
-                manager.clearLocalServices(channel,discoveryActionLisner);
-                manager.clearLocalServices(channel,serviceRequestActionListner);
-
-                manager.stopPeerDiscovery(channel, discoveryActionLisner);
-                manager.stopPeerDiscovery(channel, broadcastACtionListner);
-                manager.stopPeerDiscovery(channel, serviceRequestActionListner);
-
-                manager.clearServiceRequests(channel, broadcastACtionListner);
-                manager.clearServiceRequests(channel, discoveryActionLisner);
-                manager.clearServiceRequests(channel, serviceRequestActionListner);
-
-                startSparrowWifiService();
+//                startSparrowWifiService();
             }
         };
     }
@@ -267,7 +322,7 @@ public class Sparrow extends Service {
             timer.cancel();
             timer = null;
         }
-        releaseWifiLock();
+
     }
     /********************************TIMER********************/
 
@@ -334,56 +389,3 @@ public class Sparrow extends Service {
 
 
 }
-
-
-
-
-
-//    private List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
-//
-//    private WifiP2pManager.PeerListListener peerListListener  = new WifiP2pManager.PeerListListener() {
-//        @Override
-//        public void onPeersAvailable(WifiP2pDeviceList peerList) {
-//
-//            Collection<WifiP2pDevice> refreshedPeers = peerList.getDeviceList();
-//            if (!refreshedPeers.equals(peers)) {
-//                peers.clear();
-//                peers.addAll(refreshedPeers);
-//
-//                Log.i(TAG_SPARROW_WIFI_SERVICE, "Wifi devices found: " + peers.toString());
-//
-//            }
-//
-//            if (peers.size() == 0) {
-//                Log.i(TAG_SPARROW_WIFI_SERVICE, "No Wifi devices found");
-//                return;
-//            }
-//        }
-//    };
-//
-//
-//    /**********Discover  p2p devices****************/
-//    manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-//    channel = manager.initialize(this, getMainLooper(), null);
-//    intentFilter = new IntentFilter();
-//        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-//        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-//        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-//        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-//
-//    receiver = new WiFiDirectBroadcastReceiver(manager, channel, peerListListener);
-//    registerReceiver(receiver, intentFilter);
-//
-//
-//        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
-//        @Override
-//        public void onSuccess() {
-//        }
-//
-//        @Override
-//        public void onFailure(int reasonCode) {
-//        }
-//    });
-//
-//
-///**********Discover  p2p devices****************/
